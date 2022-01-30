9e3209cce6a7be1c383592a15d818649ab2e0688
Harry Maraziaris <sdi1800105@di.uoa.gr>
2022-01-31 00:45:07 +0200

    clean RegexMatch a bit

--- basic/RegexMatch.hs
+++ basic/RegexMatch.hs
@@ -6,38 +6,36 @@ import NFAtoDFA
 
 
 regexFullMatch :: ([Char], [Char]) -> Bool
-regexFullMatch (regStr, strPat) = search_full transitions finalStates firstState strPat
+regexFullMatch (regStr, strPat) = searchFull transitions finalStates firstState strPat
     where (_,_,transitions, firstState, finalStates) = nfaToDfa (makeNfa regStr)
-
-
--- search_full :: Transitions -> States -> StateId -> [Char] -> Bool
--- search_full transitions finalStates currState [] = is_member currState finalStates
--- search_full transitions finalStates currState (c:xs) = if nextState == [] then False else search_full transitions finalStates (head nextState) xs
---         where nextState = map (\(x,y,z) -> y) (filter (\(x,y,z) -> x == currState && z == c) transitions)
-
-is_member x [] = False
-is_member x (y:xs) = if x == y then True else is_member x xs
-
-
-regexPartMatch :: ([Char], [Char]) -> [[Char]]
-regexPartMatch (regStr, strPat) = search_part transitions finalStates firstState strPat ""
-    where (_,_,transitions, firstState, finalStates) = nfaToDfa (makeNfa regStr)
-
-
-search_part :: Transitions -> States -> StateId -> [Char] -> [Char] -> [[Char]]
-search_part transitions finalStates currState [] stringSoFar = if is_member currState finalStates then [stringSoFar] else []
-search_part transitions finalStates currState (c:xs) stringSoFar
-            | nextState == [] = if is_member currState finalStates then [stringSoFar] else []
-            | is_member currState finalStates = stringSoFar : (search_part transitions finalStates (head nextState) xs (stringSoFar ++ [c]))
-            | otherwise = search_part transitions finalStates (head nextState) xs (stringSoFar ++ [c])
-            where nextState = map (\(x,y,z) -> y) (filter (\(x,y,z) -> x == currState && z == c) transitions)
 
 -- Bonus: Wildcard
-search_full :: Transitions -> States -> StateId -> [Char] -> Bool
-search_full transitions finalStates currState [] = is_member currState finalStates
-search_full transitions finalStates currState (c:xs)
-        | nextState == [] = False
-        | length nextState == 1 = search_full transitions finalStates (head nextState) xs
-        | otherwise = if (swrap (head nextState) == True) then True else swrap (head (tail nextState)) 
-        where nextState = map (\(x,y,z) -> y) (filter (\(x,y,z) -> x == currState && (z == c || z == '.')) transitions)
-              swrap state = search_full transitions finalStates state xs
+searchFull :: Transitions -> States -> StateId -> [Char] -> Bool
+searchFull transitions finalStates currState [] = isMember currState finalStates  -- Fully consumed input string
+searchFull transitions finalStates currState (c:xs) =
+        takeUntil ( == True) . map swrap . map (\(x,y,z) -> y) . filter (\(x,y,z) -> x == currState && (z == c || z == '.')) $ transitions
+        where swrap state = searchFull transitions finalStates state xs
+
+-- Bonus: Partial Matching
+regexPartMatch :: ([Char], [Char]) -> [[Char]]
+regexPartMatch (regStr, strPat) = searchPart transitions finalStates firstState strPat ""
+    where (_,_,transitions, firstState, finalStates) = nfaToDfa (makeNfa regStr)
+
+
+searchPart :: Transitions -> States -> StateId -> [Char] -> [Char] -> [[Char]]
+searchPart transitions finalStates currState [] stringSoFar = if isMember currState finalStates then [stringSoFar] else []
+searchPart transitions finalStates currState (c:xs) stringSoFar
+            | nextState == [] = if isFinalState then [stringSoFar] else []
+            | isFinalState = stringSoFar : cont
+            | otherwise = cont
+            where nextState = map (\(x,y,z) -> y) . filter (\(x,y,z) -> x == currState && z == c) $ transitions
+                  cont = searchPart transitions finalStates (head nextState) xs (stringSoFar ++ [c])
+                  isFinalState = isMember currState finalStates
+
+isMember :: Eq t => t -> [t] -> Bool
+isMember x [] = False
+isMember x (y:xs) = (x == y) || isMember x xs
+
+takeUntil :: (t -> Bool) -> [t] -> Bool
+takeUntil f [] = False
+takeUntil f (x:xs) = f x || takeUntil f xs
