c021aec3e57edfe7c5896b377711a192eff38d35
Harry Maraziaris <sdi1800105@di.uoa.gr>
2022-02-02 16:09:09 +0200

    Polish 'basic/MakeNFA.hs' - TODO: Add mergesort

--- basic/MakeNFA.hs
+++ basic/MakeNFA.hs
@@ -4,8 +4,6 @@ module MakeNFA (makeNfa) where
 import RegParser
 import Types
 
--- type Fsa = (States, Inputs, Transitions, FirstState, LastStates)
-
 type AvailableState = StateId
 
 -- TODO: Replace with merge-sort
@@ -19,38 +17,48 @@ quicksort1 (x:xs) =
 
 createNfaRec :: AvailableState -> RegExpr -> (Fsa, AvailableState)
 
-createNfaRec n EmptyChar = ( ([x,y], [], [(x,y,'_')], x, [y]), n+2 )
-                        where (x,y) = (n,n+1)
+-- ε-transition : nfa with just one state, that is also final
+createNfaRec n EmptyChar = ( ([n], [], [], n, [n]), n+1 )
 
-createNfaRec n AnyLetter = ( ([x,y], ['.'], [(x,y,'.')], x, [y]), n+2 )
-                        where (x,y) = (n,n+1)
+createNfaRec n AnyLetter = (fsa, nextAvailState)
+    where
+      fsa = ([src,dest], ['.'], [(src,dest,'.')], src, [dest]) 
+      (src,dest,nextAvailState) = (n,n+1,n+2)
 
-createNfaRec n (Letter c) = ( ([x,y], [c], [(x,y,c)], x, [y]), n+2 )
-                        where (x,y) = (n,n+1)
+createNfaRec n (Letter c) = (fsa, nextAvailState)
+    where
+      fsa = ([src,dest], [c], [(src,dest,c)], src, [dest])
+      (src,dest,nextAvailState) = (n,n+1,n+2)
 
-createNfaRec n (Kleene reg) = ( (states, inputs', transitions, x, [y]), availableState+2 )
-        where ((states', inputs', transitions', firstState', [lastState']), availableState) = createNfaRec n reg
-              (x,y) = (availableState, availableState+1)
-              states = [1..availableState+1] -- no need to update state in createNfaRec
-              transitions = (x,y,'_') : (x,firstState','_') : (lastState',y,'_') : (lastState',firstState','_') : transitions'
+createNfaRec n (Kleene reg) = (fsa, nextAvailState)
+    where 
+      fsa = ([], [], transitions, src, [dest])
+      ((_, _, transitions', firstState', [lastState']), availState) = createNfaRec n reg
+      (src,dest,nextAvailState) = (availState, availState+1, availState+2)
+      transitions = (src,dest,'_') : (src,firstState','_') : (lastState',dest,'_') : (lastState',firstState','_') : transitions'
 
-createNfaRec n (Concat (reg1, reg2)) = ( (states, inputs, transitions, firstState1, [lastState2]), availableState2 )
-        where ((states1, inputs1, transitions1, firstState1, [lastState1]), availableState1) = createNfaRec n reg1
-              ((states2, inputs2, transitions2, firstState2, [lastState2]), availableState2) = createNfaRec availableState1 reg2
-              states = [1..availableState2-1]
-              inputs = inputs1 ++ inputs2
-              transitions = (lastState1,firstState2,'_') : (transitions1 ++ transitions2)
+createNfaRec n (Concat (reg1, reg2)) = (fsa, availState2)
+    where 
+      fsa = ([], [], transitions, firstState1, [lastState2])
+      ((_, _, transitions1, firstState1, [lastState1]), availState1) = createNfaRec n reg1
+      ((_, _, transitions2, firstState2, [lastState2]), availState2) = createNfaRec availState1 reg2
+      transitions = (lastState1,firstState2,'_') : (transitions1 ++ transitions2)
 
-createNfaRec n (Union (reg1, reg2)) = ( (states, inputs, transitions, x, [y]), availableState2+2 )
-        where ((states1, inputs1, transitions1, firstState1, [lastState1]), availableState1) = createNfaRec n reg1
-              ((states2, inputs2, transitions2, firstState2, [lastState2]), availableState2) = createNfaRec availableState1 reg2
-              (x,y) = (availableState2, availableState2+1)
-              states = [1..availableState2+1]
-              inputs = inputs1 ++ inputs2  -- not needed, can find inputs by initial str via sort + rm dupl + rm ()|* ' '
-              transitions = (x,firstState1,'_') : (x,firstState2,'_') : (lastState1,y,'_') : (lastState2,y,'_') : (transitions1 ++ transitions2)
+createNfaRec n (Union (reg1, reg2)) = (fsa, nextAvailState)
+    where 
+      fsa = ([], [], transitions, src, [dest])
+      ((_,_, transitions1, firstState1, [lastState1]), availState1) = createNfaRec n reg1
+      ((_,_, transitions2, firstState2, [lastState2]), availState2) = createNfaRec availState1 reg2
+      (src,dest,nextAvailState) = (availState2, availState2+1, availState2+2)
+      transitions = (src,firstState1,'_') : (src,firstState2,'_') : (lastState1,dest,'_') : (lastState2,dest,'_') : (transitions1 ++ transitions2)
+
+createNfaRec _ _ = error "createNfaRec: Not a regex given"
+
 
 makeNfa :: [Char] -> Fsa
-makeNfa str = (states, inputs, t, f, [l])
-        where ((s, inputs', t, f, [l]), n) = createNfaRec 1 (parseRegexpr str)
-              inputs = quicksort1 inputs'
-              states = [1..n-1]
+makeNfa str = (states, inputs, transitions, firstState, lastStates)
+    where 
+      ((_, _, transitions, firstState, lastStates), nextAvailState) = createNfaRec 1 (parseRegexpr str)
+      states = [1..nextAvailState-1]
+      inputs = quicksort1 (filter isValidInput str)
+      isValidInput c = not ( c == '_' || c == '|' || c == '(' || c == ')' || c == '*' || c == ' ' )
